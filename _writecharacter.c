9744767391cd05b42db203d284800551eb3b5294
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-20 16:28:41 +0000

    print fixing issues

--- _writecharacter.c
+++ _writecharacter.c
@@ -9,7 +9,20 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char my_buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &my_buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		my_buf[i] = c;
+		i++;
+	}
+	return (1);
 }
 /**
  * _puts - prints a string
@@ -23,4 +36,53 @@ int _puts(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	return (i);
+}
+
+int p_int(int number)
+{
+	int size, digit, result;
+	long c, sign;
+
+	sign = 1;
+	digit = 0;
+	size = 1;
+	c = number;
+	result = 0;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		sign = -1;
+		c *= sign;
+		result++;
+	}
+
+	for (; c >= 10; size++)
+	{
+		c = c / 10;
+	}
+
+	c = sign * (long)number;
+
+	while (size >= 2)
+	{
+		digit = (c / exponent(10, size - 1));
+		_putchar(digit + '0');
+		c = c % exponent(10, size - 1);
+		size--;
+		result++;
+	}
+	_putchar(c % 10 + '0');
+	result++;
+	return (result);
+}
+
+int exponent(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * exponent(x, y - 1));
 }
