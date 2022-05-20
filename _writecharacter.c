f273eb0ba70b4b9a044f8ff60702e5a05b493df4
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-20 01:00:34 +0000

    0 task complete with betty

--- _writecharacter.c
+++ _writecharacter.c
@@ -11,3 +11,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * _puts - prints a string
+ * @str: pointer to the string
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
