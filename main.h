933796d9cc9dae317dbf80332c5a92bd272a1d59
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-21 10:24:39 +0000

    finishing task 0

--- main.h
+++ main.h
@@ -1,27 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <stdarg.h>
-int get_action(char c, va_list *ap);
 
 int exponent(int x, int y);
-int p_int(int);
-int _puts(char *str);
+int (*_select_action(char c))(va_list);
+int _printchar(va_list args);
+int _print_int(va_list args);
+int _print_str(va_list args);
 int _putchar(char c);
-int _printf(const char *format, ...);
-
-int mkstring(va_list *);
-int mkchar(va_list *);
-int mkint(va_list *);
-
-/**
- * struct flag - flag object
- * @letter: flag char
- * @prnt: print function pointer
- * Descriptions: contains flag letter and corresponding prnt funct
- */
-typedef struct flag
-{
-	char letter;
-	int (*prnt)(va_list *);
-} find_action;
+int _printf(const char * const format, ...);
 #endif
--- printf.c
+++ printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stddef.h>
 /**
  * _printf - mimics printf function
  * @format: string to print out format
@@ -8,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int index = 0, display_count = 0;
+	int index = 0, display_count = 0, (*func)(va_list);
 
 	va_start(args, format);
 
@@ -17,18 +18,22 @@ int _printf(const char *format, ...)
         if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 
-	for (index = 0; format[index] != '\0'; index++)
+	while(format[index] != '\0')
 	{
-		if (format[index] == '%')
-		{
-			display_count += get_action(format[index + 1], &args);
-			display_count++;
-		}
-		else
-		{
-			_putchar(format[index]);
-			display_count++;
-		}
+        if (format[index] == '%' && format[index + 1] != '%')
+        {
+            func = _select_action(format[index + 1]);
+            if (func != NULL)
+            {
+                display_count += func(args);
+                index++;
+            }
+        }
+        else
+        {
+            display_count += _putchar(format[index]);
+        }
+        index++;
 	}
 	va_end(args);
 	return (display_count);
