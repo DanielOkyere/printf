d6c381e3057a1e02df13f408bb4d9c090a8999c8
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-05-22 17:12:28 +0000

    final commit

--- select_func.c
+++ select_func.c
@@ -19,12 +19,15 @@ int (*_select_action(char s))(va_list, t_flags *)
 		{'o', print_octal},
 		{'u', _print_unsigned},
 		{'r', print_rev},
-		{'%', _printchar}
+		{'%', _printchar},
+		{'R', _print_rot13},
+		{'S', _print_bigS},
+		{'p', _print_address}
 	};
 
 	register int i;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 14; i++)
 	{
 		if (func_arr[i].c == s)
 			return (func_arr[i].f);
